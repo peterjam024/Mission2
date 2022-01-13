2e2918edd858b49a486773d2022da1de23853927
James Peterson <jamespetersonwrestling@gmail.com>
2022-01-12 23:25:05 -0700

    Add project files.

---
+++ wwwroot/lib/jquery/Default.js
@@ -0,0 +1,21 @@
+﻿$("#GetGrades").click(function () {
+
+    var Assignment
+    var Group
+    var Quiz
+    var Exam
+    var Intex
+    var OverallGrade
+
+    Assignment = parseFloat(document.getElementById("Assignments"));
+    Group = parseFloat(document.getElementById("Group Projects"));
+    Quiz = parseFloat(document.getElementById("Quizzes"));
+    Exam = parseFloat(document.getElementById("Exams"));
+    Intex = parseFloat(document.getElementById("INTEX"));
+
+    OverallGrade = Assignment * .55 + Group * .05 + Quiz * .1 + Exam * .2 + Intex * .1;
+    alert(OverallGrade);
+
+
+})
+
